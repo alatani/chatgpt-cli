7fd411cbdef627be7eaf6874eda1b1eb09c87dd0
narita-atsuhiro <alayashiki@gmail.com>
2023-03-25 13:16:32 +0900

    refactor(wip)

--- chatgpt.py
+++ chatgpt.py
@@ -1,4 +1,5 @@
 #!/bin/env python
+from __future__ import annotations
 
 import atexit
 import click
@@ -6,9 +7,14 @@ import os
 import requests
 import sys
 import yaml
+import fire
+from dataclasses import dataclass
+from dataclasses_json import dataclass_json
 
 from prompt_toolkit import PromptSession, HTML
 from prompt_toolkit.history import FileHistory
+from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
+from rich.panel import Panel
 from rich.console import Console
 
 CONFIG_FILE = "config.yaml"
@@ -30,155 +36,236 @@ completion_tokens = 0
 # Initialize the console
 console = Console()
 
+@dataclass_json
+@dataclass
+class Message:
+    role: str
+    content: str
 
-def load_config(config_file: str) -> dict:
+    @classmethod
+    def from_dict(cls, message: dict) -> Message:
+        return cls(**message)
+
+    @classmethod
+    def to_list(cls, messages: list[Message]) -> list[dict]:
+        return [message.to_dict() for message in messages]
+
+
+@dataclass
+class ChatContext:
+    file_path: str
+    messages: list[Message] = list
+    """
+    ---
+    %%
+    {
+    }
+    %%
+    """
+
+    def resolve(self) -> list[Message]:
+        return self.messages
+        raise NotImplementedError()
+        with open(self.file_path) as f:
+            self.message = []
+
+        return self.messages
+
+    def append(self, message: Message) -> None:
+        self.messages.append(message)
+        self._flush()
+
+    def _flush(self) -> None:
+        raise NotImplementedError()
+        with open(self.file_path, "w") as f:
+            yaml.dump(self.messages, f)
+
+
+
+def load_config(config_file: str, profile: str = "defualt") -> dict:
     """
     Read a YAML config file and returns it's content as a dictionary
     """
     with open(config_file) as file:
         config = yaml.load(file, Loader=yaml.FullLoader)
-    if not config["api-key"].startswith("sk"):
+
+    if not config.get("api-key", "").startswith("sk"):
         config["api-key"] = os.environ.get("OAI_SECRET_KEY", "fail")
-    while not config["api-key"].startswith("sk"):
+
+    if not config.get("api-key", "").startswith("sk"):
+        keyfile_path = os.path.expanduser(config["profiles"]["defualt"]["keyfile"])
+        with open(keyfile_path) as keyfile:
+            config["api-key"] = keyfile.read()
+
+    profile = "defualt"
+    while not config.get("api-key", "").startswith("sk"):
         config["api-key"] = input(
             "Enter your OpenAI Secret Key (should start with 'sk-')\n"
         )
+
     return config
 
 
-def calculate_expense(
-    prompt_tokens: int,
-    completion_tokens: int,
-    prompt_pricing: float,
-    completion_pricing: float,
-) -> float:
-    """
-    Calculate the expense, given the number of tokens and the pricing rates
-    """
-    expense = ((prompt_tokens / 1000) * prompt_pricing) + (
-        (completion_tokens / 1000) * completion_pricing
-    )
-    return round(expense, 6)
+@dataclass
+class ChatGPTResponse:
+    message: Message
+    prompt_tokens: int
+    completion_tokens: int
 
+@dataclass
+class ChatGPTClient:
+    config: dict
 
-def display_expense(model) -> None:
-    """
-    Given the model used, display total tokens used and estimated expense
-    """
-    total_expense = calculate_expense(
-        prompt_tokens,
-        completion_tokens,
-        PRICING_RATE[model]["prompt"],
-        PRICING_RATE[model]["completion"],
-    )
-    console.print(f"Total tokens used: [green bold]{prompt_tokens + completion_tokens}")
-    console.print(f"Estimated expense: [green bold]${total_expense}")
+    def get_response(self, messages: list[Message]) -> ChatGPTResponse:
+        body = {"model": self.config["model"], "messages": Message.to_list(messages)}
 
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.config['api-key']}",
+        }
+        # return ChatGPTResponse(
+        #     Message(role="assistant", content="OK"),
+        #     20,10
+        # )
 
-def start_prompt(session, config):
-    # TODO: Refactor to avoid a global variables
-    global prompt_tokens, completion_tokens
-
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {config['api-key']}",
-    }
-
-    message = session.prompt(HTML(f"<b>[{prompt_tokens + completion_tokens}] >>> </b>"))
-
-    if message.lower() == "/q":
-        raise EOFError
-    if message.lower() == "":
-        raise KeyboardInterrupt
-
-    messages.append({"role": "user", "content": message})
-
-    body = {"model": config["model"], "messages": messages}
-
-    try:
-        r = requests.post(
-            f"{BASE_ENDPOINT}/chat/completions", headers=headers, json=body
-        )
-    except requests.ConnectionError:
-        console.print("Connection error, try again...", style="red bold")
-        messages.pop()
-        raise KeyboardInterrupt
-    except requests.Timeout:
-        console.print("Connection timed out, try again...", style="red bold")
-        messages.pop()
-        raise KeyboardInterrupt
-
-    if r.status_code == 200:
-        response = r.json()
-
-        message_response = response["choices"][0]["message"]
-        usage_response = response["usage"]
-
-        console.print(message_response["content"].strip())
-
-        # Update message history and token counters
-        messages.append(message_response)
-        prompt_tokens += usage_response["prompt_tokens"]
-        completion_tokens += usage_response["completion_tokens"]
-
-    elif r.status_code == 400:
-        response = r.json()
-        if "error" in response:
-            if response["error"]["code"] == "context_length_exceeded":
-                console.print("Maximum context length exceeded", style="red bold")
-                raise EOFError
-                # TODO: Develop a better strategy to manage this case
-        console.print("Invalid request", style="bold red")
-        raise EOFError
-
-    elif r.status_code == 401:
-        console.print("Invalid API Key", style="bold red")
-        raise EOFError
-
-    elif r.status_code == 429:
-        console.print("Rate limit or maximum monthly limit exceeded", style="bold red")
-        messages.pop()
-        raise KeyboardInterrupt
-
-    else:
-        console.print(f"Unknown error, status code {r.status_code}", style="bold red")
-        console.print(r.json())
-        raise EOFError
-
-
-@click.command()
-@click.option(
-    "-c", "--context", "context", type=click.File("r"), help="Path to a context file"
-)
-def main(context) -> None:
-    history = FileHistory(".history")
-    session = PromptSession(history=history)
-
-    try:
-        config = load_config(CONFIG_FILE)
-    except FileNotFoundError:
-        console.print("Configuration file not found", style="red bold")
-        sys.exit(1)
-
-    #Run the display expense function when exiting the script
-    atexit.register(display_expense, model=config["model"])
-
-    console.print("ChatGPT CLI", style="bold")
-    console.print(f"Model in use: [green bold]{config['model']}")
-
-    # Context from the command line option
-    if context:
-        console.print(f"Context file: [green bold]{context.name}")
-        messages.append({"role": "system", "content": context.read().strip()})
-
-    while True:
         try:
-            start_prompt(session, config)
-        except KeyboardInterrupt:
-            continue
-        except EOFError:
-            break
+            r = requests.post(
+                f"{BASE_ENDPOINT}/chat/completions", headers=headers, json=body
+            )
+        except requests.ConnectionError:
+            console.print("Connection error, try again...", style="red bold")
+            messages.pop()
+            raise KeyboardInterrupt
+        except requests.Timeout:
+            console.print("Connection timed out, try again...", style="red bold")
+            messages.pop()
+            raise KeyboardInterrupt
 
+        if r.status_code == 200:
+            response = r.json()
+
+            message_response = response["choices"][0]["message"]
+            usage_response = response["usage"]
+
+            return ChatGPTResponse(
+                Message(role="assistant", content=message_response['content'].strip()),
+                usage_response["prompt_tokens"],
+                usage_response["completion_tokens"]
+            )
+
+        elif r.status_code == 400:
+            response = r.json()
+            if "error" in response:
+                if response["error"]["code"] == "context_length_exceeded":
+                    console.print("Maximum context length exceeded", style="red bold")
+                    raise EOFError
+                    # TODO: Develop a better strategy to manage this case
+            console.print("Invalid request", style="bold red")
+            raise EOFError
+
+        elif r.status_code == 401:
+            console.print("Invalid API Key", style="bold red")
+            raise EOFError
+
+        elif r.status_code == 429:
+            console.print("Rate limit or maximum monthly limit exceeded", style="bold red")
+            messages.pop()
+            raise KeyboardInterrupt
+
+        else:
+            console.print(f"Unknown error, status code {r.status_code}", style="bold red")
+            console.print(r.json())
+            raise EOFError
+
+@dataclass
+class Expense:
+    config: dict
+    prompt_tokens: int = 0
+    completion_tokens: int = 0
+
+    def add(self, prompt_tokens: int, completion_tokens: int):
+        self.prompt_tokens += prompt_tokens
+        self.completion_tokens += completion_tokens
+
+    def display(self, model) -> None:
+        """
+        Given the model used, display total tokens used and estimated expense
+        """
+        total_expense = self.__calculate_expense(
+            self.prompt_tokens,
+            self.completion_tokens,
+            PRICING_RATE[model]["prompt"],
+            PRICING_RATE[model]["completion"],
+        )
+        console.print(f"Total tokens used: [green bold]{self.prompt_tokens + self.completion_tokens}")
+        console.print(f"Estimated expense: [green bold]${total_expense}")
+
+    def __calculate_expense(self,
+        prompt_tokens: int,
+        completion_tokens: int,
+        prompt_pricing: float,
+        completion_pricing: float,
+    ) -> float:
+        """
+        Calculate the expense, given the number of tokens and the pricing rates
+        """
+        expense = ((prompt_tokens / 1000) * prompt_pricing) + (
+            (completion_tokens / 1000) * completion_pricing
+        )
+        return round(expense, 6)
+
+
+class ChatGptCli:
+    def __init__(self, context: str|None = None):
+        self.context = context
+
+    def run(self):
+        history = FileHistory(".history")
+        session = PromptSession(history=history, multiline=True, auto_suggest=AutoSuggestFromHistory())
+
+        # try:
+        config = load_config(CONFIG_FILE)
+        print(config["api-key"])
+
+        #Run the display expense function when exiting the script
+        expence = Expense(config)
+        atexit.register(expence.display, model=config["model"])
+
+        console.print("ChatGPT CLI", style="bold")
+        console.print(f"Model in use: [green bold]{config['model']}")
+        messages = []
+
+        # Context from the command line option
+        if self.context:
+            console.print(f"Context file: [green bold]{self.context}")
+            with open(os.path.expanduser(self.context)) as file:
+                messages.append(Message(role="assistant", content=file.read().strip()))
+
+
+        chat_gpt_client = ChatGPTClient(config)
+        while True:
+            try:
+                input_message = session.prompt(HTML(f"<b>[{prompt_tokens + completion_tokens}] >>> </b>"))
+
+                if input_message.strip().lower() == "/q":
+                    raise EOFError
+                if input_message.strip().lower() == "":
+                    raise KeyboardInterrupt
+
+                messages.append(Message(role="user", content=input_message))
+                console.log(messages)
+
+                response: ChatGPTResponse = chat_gpt_client.get_response(messages)
+
+                console.print(response.message.content)
+
+                messages.append(response.message)
+                expence.add(response.prompt_tokens, response.completion_tokens)
+            except KeyboardInterrupt:
+                messages.pop()
+                continue
+            except EOFError:
+                break
 
 if __name__ == "__main__":
-    main()
+    fire.Fire(ChatGptCli)
